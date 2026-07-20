ab10f201247b87b7b23513837c3887065cbb1b86
Aman Chouhan <amanc7477@gmail.com>
2026-07-20 23:27:57 +0530

    basic questions added

--- Day_01_Series_DataFrame/scripts/practice/part_2.py
+++ Day_01_Series_DataFrame/scripts/practice/part_2.py
@@ -15,16 +15,16 @@ file_path = (
 
 orders = pd.read_csv(file_path)
 
-# print(orders.head())
+print(orders.head())
 
-# print(orders.columns)
+print(orders.columns)
 
-# print(orders[["city", "area"]])
+print(orders[["city", "area"]])
 
-# print(orders[["city","area", "customer_segment"]].groupby(["city", "area"]).count())
+print(orders[["city","area", "customer_segment"]].groupby(["city", "area"]).count())
 
-# print(orders[orders["city"] == "Delhi"])
+print(orders[orders["city"] == "Delhi"])
 
-# print(orders[orders["city"].isin(["Delhi", "Mumbai"])])
+print(orders[orders["city"].isin(["Delhi", "Mumbai"])])
 
 print(orders[(orders["city"] == "Delhi") & (orders["customer_segment"] == "Consumer")])
